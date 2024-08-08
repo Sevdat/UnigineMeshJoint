315f3ff1675672f9f7706b20b26f5c6d06abf1d0
Sevdat <55320710+Sevdat@users.noreply.github.com>
2024-08-08 22:43:14 +0200

    Update FreePointEnviroment.cs

--- v1/FreePointEnviroment.cs
+++ v1/FreePointEnviroment.cs
@@ -12,13 +12,18 @@ public class FreePointEnviroment : Component
     public class World {
         public Dictionary<string,BodyInWorld.BodyData> allBodies;
         public Library library = new();
+        public BodyInWorld bodyInWorld = new();
 
         public class Path {
+            public Path() {}
             public string name;
             public int? jointIndex = default;
             public int? meshVertexIndex = default;
-            public Path() {}
-
+            public Path(string name,int? jointIndex,int? meshVertexIndex){
+                this.name = name;
+                this.jointIndex = jointIndex;
+                this.meshVertexIndex = meshVertexIndex;
+            }
             public string pathToString() {
                 string temp = $"{name}";
                 bool jointCheck = jointIndex != null;
@@ -30,33 +35,50 @@ public class FreePointEnviroment : Component
                     };
                 return temp;
             }
-
-            public Path(string name,int? jointIndex,int? meshVertexIndex){
-                this.name = name;
-                this.jointIndex = jointIndex;
-                this.meshVertexIndex = meshVertexIndex;
+            public Path createPath(string name,int? jointIndex,int? meshVertexIndex){
+                Path temp;
+                bool error = jointIndex == null && meshVertexIndex != null;
+                temp = (!error)? new Path(name,jointIndex,meshVertexIndex):null;
+                return temp;
             }
-        }
-        public Path createPath(string name,int? jointIndex,int? meshVertexIndex){
-            Path temp;
-            bool error = jointIndex == null && meshVertexIndex != null;
-            temp = (!error)? new Path(name,jointIndex,meshVertexIndex):null;
-            return temp;
         }
 
         public class BodyInWorld {
 
             public Axis axis;
             public struct Axis {
-                public vec3 origin,x,y,z;
+                public vec3 origin,x,y,z ;
                 public Axis(vec3 origin,vec3 x,vec3 y,vec3 z){
                     this.origin = origin;
                     this.x = x;
                     this.y = y;
                     this.z = z;
                 }
-                public Axis newAxis(vec3 origin,vec3 x,vec3 y,vec3 z){
+                public Axis get(){
                     return new Axis(origin,x,y,z);
+                }
+                public Axis create(vec3 origin,float distanceFromOrigin){
+                    this.origin=origin;
+                    scale(distanceFromOrigin);
+                    return new Axis(origin,x,y,z);
+                }
+                public void scale(float distanceFromOrigin){
+                    bool gateX = x == vec3.ZERO;
+                    bool gateY = y == vec3.ZERO;
+                    bool gateZ = z == vec3.ZERO;
+                    x = gateX?
+                        origin + new vec3(distanceFromOrigin,0,0):
+                        origin + direction(x,origin)*distanceFromOrigin;
+                    y = gateY?
+                        origin + new vec3(0,distanceFromOrigin,0):
+                        origin + direction(y,origin)*distanceFromOrigin;
+                    z = gateZ?
+                        origin + new vec3(0,0,distanceFromOrigin):
+                        origin + direction(z,origin)*distanceFromOrigin;                                               
+                }
+                public vec3 direction(vec3 point,vec3 origin){ 
+                    vec3 v = point-origin;
+                    return v/ MathLib.Length(v);
                 }
             }
 
@@ -68,11 +90,10 @@ public class FreePointEnviroment : Component
                     this.globalAxis = globalAxis;
                     this.bodyStructure = bodyStructure;
                 }
-                public BodyData newBodyData(Axis globalAxis,Dictionary<int,Joint> bodyStructure){
+                public BodyData create(Axis globalAxis,Dictionary<int,Joint> bodyStructure){
                     return new BodyData(globalAxis,bodyStructure);
                 }
             }
-
 
             public Joint joint;
             public struct Joint {
@@ -84,20 +105,20 @@ public class FreePointEnviroment : Component
                     this.localAxis = localAxis;
                     this.mesh = mesh;
                 }
-                public Joint newJoint(List<int> jointConnections,Axis localAxis,BodyMesh mesh){
+                public Joint create(List<int> jointConnections,Axis localAxis,BodyMesh mesh){
                     return new Joint(jointConnections,localAxis,mesh);
                 }
             }
 
             public Triangle triangle;
-            public struct Triangle{
+            public struct Triangle {
                 public int a,b,c;
                 public Triangle(int a,int b,int c){
                     this.a = a;
                     this.b = b;
                     this.c = c;
                 }
-                public Triangle newTriangle(int a,int b,int c){
+                public Triangle create(int a,int b,int c){
                     return new Triangle(a,b,c);
                 }
             }
@@ -110,16 +131,15 @@ public class FreePointEnviroment : Component
                     this.vertex = vertex;
                     this.indices = indices;
                 }
-                public BodyMesh newBodyMesh(List<vec3> vertex,List<Triangle> indices){
+                public BodyMesh create(List<vec3> vertex,List<Triangle> indices){
                     return new BodyMesh(vertex,indices);
                 }
             }
         }
 
         public class Library {
-            public BodyInWorld bodyInWorld = new();
+            
             public void lol(){
-                bodyInWorld.axis.newAxis(0,0,0,0);
             }
             public class BodyClass: BodyInWorld {
                 public Quaternion quaternion = new();
@@ -160,8 +180,17 @@ public class FreePointEnviroment : Component
 
 	void Init()
 	{
-        world.library.codeTest();
-
+        var v = world.bodyInWorld.axis.create(new vec3(5,5,5),2);
+        Log.Message(v.origin +"\n");
+        Log.Message(v.x+"\n");
+        Log.Message(v.y+"\n");
+        Log.Message(v.z+"\n");
+        v.create(new vec3(5,5,5),5);
+        Log.Message(v.origin +"\n");
+        Log.Message(v.x+"\n");
+        Log.Message(v.y+"\n");
+        Log.Message(v.z+"\n");
+        
 	}
 
 	void Update()
